be32abea314459e460b6325b1086525377b0dad2
ngthuc <lenguyenthuc.edu@gmail.com>
2023-01-11 16:43:54 +0700

    Update code

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const redirectUri = `${window.location.protocol}//${window.location.host}`;
 const profileScopes = [ZALO_AUTH_SCOPES.USER_ID_BY_APP, ZALO_AUTH_SCOPES.DISPLAY_NAME, ZALO_AUTH_SCOPES.LARGE_PICTURE];
 
 function handleLogin() {
+	showLoading();
 	const refreshToken = sessionStorage.getItem('zalo_refresh_token');
 	if (!refreshToken) {
 		zaloAuth
@@ -21,6 +22,10 @@ function handleLogin() {
 			})
 			.catch((err) => {
 				console.error('Login error: ', err);
+			})
+			.finally(() => {
+				console.log('Login done, hide loading');
+				hideLoading();
 			});
 		return;
 	}
@@ -45,6 +50,10 @@ function handleRefreshSession() {
 		})
 		.catch((err) => {
 			console.error('Refresh session error: ', err);
+		})
+		.finally(() => {
+			console.log('Refresh session done, hide loading');
+			hideLoading();
 		});
 }
 
@@ -56,7 +65,7 @@ function handleLogout() {
 }
 
 function handleUserProfile() {
-	const beforeLoginEl = document.getElementById('login-button');
+	const beforeLoginEl = document.getElementById('sign-in');
 	const afterLoginEl = document.getElementById('login-success');
 	const userProfile = sessionStorage.getItem('zalo_user_profile') ? JSON.parse(sessionStorage.getItem('zalo_user_profile')) : null;
 
@@ -68,5 +77,42 @@ function handleUserProfile() {
 		afterLoginEl.style.display = 'block';
 		document.getElementById('welcome').innerText = `Welcome ${userProfile.displayName}`;
 		document.getElementById('user-avatar').src = userProfile.photoUrl;
+	}
+}
+
+function createLoadingElement(parentId = 'root') {
+	const rootEl = document.getElementById(parentId);
+	const loadingEl = document.createElement('div');
+	loadingEl.id = 'loading-overlay';
+	loadingEl.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+	loadingEl.style.position = 'fixed';
+	loadingEl.style.top = '0';
+	loadingEl.style.left = '0';
+	loadingEl.style.right = '0';
+	loadingEl.style.bottom = '0';
+	loadingEl.style.margin = 'auto';
+	loadingEl.style.width = '100%';
+	loadingEl.style.height = '100%';
+
+	// Insert loading overlay as a first child
+	rootEl.insertBefore(loadingEl, rootEl.firstChild);
+}
+
+function showLoading() {
+	createLoadingElement('root');
+}
+
+function visibleLoading() {
+	createLoadingElement('root');
+
+	setTimeout(() => {
+		hideLoading();
+	}, 3000);
+}
+
+function hideLoading() {
+	const loadingEl = document.getElementById('loading-overlay');
+	if (loadingEl) {
+		loadingEl.remove();
 	}
 }
